383bad3caf2120fbac26347722c7a9de0c8ee889
Kyle Smeaton <smekylg@dunwoody.edu>
2017-09-14 22:29:35 -0500

    added validation and started for loop

--- Assignment2Point4/Program.cs
+++ Assignment2Point4/Program.cs
@@ -20,28 +20,29 @@ namespace Assignment2Point4
     {
         static void Main(string[] args)
         {
-            string input;
-            int result = -1;
+            string choice;
+            int result;
 
             do
             {
+                result = -1;
                 //debug
                 //WriteLine("\n\nresult @ top of do is {0}\n\n", result);
 
                 WriteLine("Enter 1, 2, or 3 to select your option, then press Enter: \n1. Calculate sum of scores\n2. Enter customer purchases\n3. Quit");
-                input = ReadLine();
+                choice = ReadLine();
                 try
                 {
-                    result = Convert.ToInt32(input);
+                    result = Convert.ToInt32(choice);
                 }
                 catch (FormatException)
                 {
-                    WriteLine("\n'{0}' is not a whole number. Enter a whole number please.\n", input);
+                    WriteLine("\n'{0}' is not a whole number. Enter a whole number please.\n", choice);
                 }
                 catch (Exception)
                 {
                     WriteLine("PLACEHOLDER - enter selection again");
-                    input = ReadLine();
+                    choice = ReadLine();
                     result = -1;
                 }
                 if ((result != -1) && (result != 1) && (result != 2) && (result != 3))
@@ -50,7 +51,7 @@ namespace Assignment2Point4
                 }
 
                 //debug
-                //WriteLine("\n\nresult above top of if is {0}\n\n", result);
+                WriteLine("\n\nresult above top of if is {0}\n\n", result);
 
                 if (result == 1)
                 {
@@ -107,7 +108,43 @@ namespace Assignment2Point4
                 if (result == 2)
                 {
                     // temp
-                    WriteLine("if == 2 is being called.");
+                    // WriteLine("if == 2 is being called.");
+                    string compName;
+                    string strCount;
+                    int intCount;
+                    bool validIn = false;
+
+                    Write("\nWhat is the company name? ");
+                    compName = ReadLine();
+
+                    
+
+                    do
+                    {
+                        Write("How many items from 1 - 10 did {0} order? ", compName);
+                        strCount = ReadLine();
+                        try 
+                        {
+                            intCount = Convert.ToInt32(strCount);
+                            validIn = true;
+                        }
+                        catch (Exception)
+                        {
+                            WriteLine("Enter a whole number please.\n");
+                        }
+
+                    }
+                    while (validIn == false);
+
+                    for (int i = 0; i < intCount; i++)
+                    {
+                        string model;
+                        string description;
+                        string department;
+
+
+                    }
+
                 }
                 //debug
                 //WriteLine("\n\nresult @ bottom of do is {0}\n\n", result);
